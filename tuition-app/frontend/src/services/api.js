b0e3587094744779587f3e535233fd0bd3ff8c14
rushadgaidhane <rushadigaidhane@gmail.com>
2026-05-02 01:38:34 +0530

    update api url for production

--- tuition-app/frontend/src/services/api.js
+++ tuition-app/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://brightminds-backend.onrender.com/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
